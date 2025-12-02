817a8bf3efb7bc07988005056f014b3e51d143d4
mark-elias <melias23@proton.me>
2025-12-02 00:13:08 -0800

    make create-user dto

---
+++ src/user/DTOs/create-user.dto.ts
@@ -0,0 +1,22 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  MinLength,
+  IsOptional,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  username?: string;
+}

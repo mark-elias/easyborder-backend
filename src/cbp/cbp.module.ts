a0e1f225ecf2befb7d9bfa311f9c03778be914a9
mark-elias <melias23@proton.me>
2026-02-26 22:57:03 -0800

    update cbp module imports

--- src/cbp/cbp.module.ts
+++ src/cbp/cbp.module.ts
@@ -5,16 +5,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { HttpModule } from '@nestjs/axios';
 import { CbpController } from './cbp.controller';
 import { CbpService } from './cbp.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CbpScheduler } from './cbp.scheduler';
 
 @Module({
   imports: [
     HttpModule,
+    ScheduleModule.forRoot(),
     MongooseModule.forFeature([
       { name: WaitTime.name, schema: WaitTimeSchema },
       { name: Crossing.name, schema: CrossingSchema },
     ]),
   ],
-  providers: [CbpService],
+  providers: [CbpService, CbpScheduler],
   exports: [CbpService],
   controllers: [CbpController],
 })

db40379c587255675de78110aab6baf0e746b5cc
mark-elias <melias23@proton.me>
2026-05-15 02:04:37 -0700

    add rate limiting, authguard to cbp.controller

--- src/cbp/cbp.controller.ts
+++ src/cbp/cbp.controller.ts
@@ -1,18 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { CbpService } from './cbp.service';
+import { Throttle } from '@nestjs/throttler';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('cbp')
 export class CbpController {
   constructor(private cbpService: CbpService) {}
 
-  // endpint for testing cbp api call
-  @Get('test')
-  async fetch() {
+  // endpint for manually triggering CBP API fetch
+  @UseGuards(JwtAuthGuard)
+  @Throttle({ default: { limit: 3, ttl: 60000 } }) // max 3 req per min
+  @Get()
+  async triggerFetch() {
     try {
       await this.cbpService.fetchWaitTimes();
-      return { message: 'wait times from cbp api have been fetched' };
+      return { message: '✅ cbp data fetched succesfully' };
     } catch {
-      return { message: 'failed to fetch wait times from cbp api' };
+      return { message: '🚨 failed to fetch cbp data' };
     }
   }
 }

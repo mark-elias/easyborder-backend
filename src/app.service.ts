1b1d3bff5b91d2c73698c2f4e5288e260a39aefe
mark-elias <melias23@proton.me>
2026-05-15 03:12:20 -0700

    update appservice status check message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! 🐳 my app is now Dockerized!! ✅ Github actions is setup ';
+    return 'hello world ... easyborder status check';
   }
 }

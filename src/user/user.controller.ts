c01de276696f06a85ecd5fb1d7c95445d48b8f71
mark-elias <melias23@proton.me>
2026-05-15 02:43:26 -0700

    remove unused method in user.controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './DTOs/create-user.dto';
-import { User } from './schemas/user.schema';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
-
-  @Post()
-  createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.userService.create(createUserDto);
-  }
 }

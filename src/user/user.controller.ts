ad5c2aacdbc5816bd1a6f671dc97cfdd4ad52375
mark-elias <melias23@proton.me>
2026-06-26 00:50:03 -0700

    add addFavorite and removeFavorite endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Delete,
+  Body,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { User } from './schemas/user.schema';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('favorites')
+  async addFavorite(
+    @Request() req: { user: User },
+    @Body()
+    body: { crossingId: string; laneCategory: string; laneType: string },
+  ) {
+    return this.userService.addFavoriteWaitTime(
+      req.user._id.toString(),
+      body.crossingId,
+      body.laneCategory,
+      body.laneType,
+    );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('favorites')
+  async removeFavorite(
+    @Request() req: { user: User },
+    @Body()
+    body: { crossingId: string; laneCategory: string; laneType: string },
+  ) {
+    return this.userService.removeFavoriteWaitTime(
+      req.user._id.toString(),
+      body.crossingId,
+      body.laneCategory,
+      body.laneType,
+    );
+  }
 }
